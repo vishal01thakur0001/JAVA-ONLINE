6758d10ebabbaf15a46c65e5d246946d872fd1a0
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2026-07-27 13:16:31 +0530

    COMMIT

---
+++ Basic_Question/sum.java
@@ -0,0 +1,10 @@
+package Basic_Question;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
---
+++ basic/sum.java
@@ -0,0 +1,10 @@
+package basic;
+
+public class sum {
+    public static void main(String[] args) {
+        int a = 5;
+        int b = 10;
+        int sum = a + b;
+        System.out.println("The sum of " + a + " and " + b + " is: " + sum);
+    }
+}
