b423691ab013035fff282efdd8a0e11c311d994a
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2026-08-18 22:47:38 +0530

    commit

---
+++ BasicToAdavnce/SumNaturalNumbers.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class SumNaturalNumbers {
+     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value : ");
+        int n = sc.nextInt();
+        
+        int sum = 0;
+        for(int i = 1; i <= n; i++){
+            sum = sum + i;
+        }
+        System.out.print(sum);
+        sc.close();
+
+     }
+}
