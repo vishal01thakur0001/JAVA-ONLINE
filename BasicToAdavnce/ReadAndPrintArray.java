7e29ef581776c529fe6b026f6d9bbb7ba0e08361
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2026-08-20 23:52:45 +0530

    commit

---
+++ BasicToAdavnce/ReadAndPrintArray.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+public class ReadAndPrintArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of elements : ");
+        int n = sc.nextInt();
+         int[] arr = new int[n];
+         System.out.println("Enter " + n + " elements");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Array elemnts are : ");
+        for(int i = 0; i < n; i++){
+            System.out.println(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
