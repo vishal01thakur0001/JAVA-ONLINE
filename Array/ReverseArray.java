7d68c6e29ea26af978f945d6f7bf9656f2341341
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2026-08-22 01:34:07 +0530

    commit

---
+++ Array/ReverseArray.java
@@ -0,0 +1,22 @@
+package Array;
+import java.util.Scanner;
+public class ReverseArray {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of Elements : ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter " + n + " Elements : ");
+        for(int i = 0; i < n; i++){
+            arr[i] = sc.nextInt();
+        }
+        System.out.println("Reverse Array : ");
+
+        for(int i = n - 1; i >= 0; i--){
+            System.out.println(arr[i] + " ");
+        }
+        sc.close();
+    }
+}
