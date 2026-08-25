284d45d2b0ef7554e132502aff87a3bbbdfd4407
vishal01thakur0001 <vishal01thakur0001@gmail.com>
2026-08-25 19:17:30 +0530

    commit

---
+++ Array/RemoveDuplicates.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class RemoveDuplicates {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the size of array: ");
+        int n = sc.nextInt();
+
+        int[] arr = new int[n];
+
+        System.out.println("Enter the elements:");
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
+
+       
